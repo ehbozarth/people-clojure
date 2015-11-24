718470a6a5ecc650146c5957b81ff0d32609e60f
earlbozarth <ehbbozarth@gmail.com>
2015-11-24 11:37:45 -0500

    Hash mapping/keywords/filtering

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,7 +1,29 @@
 (ns people-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (let [people (slurp "people.csv")                         ;slur reads in file
+        people (str/split-lines people)                     ;str is set from top in requirements split into seperate string lines
+        people (map (fn [line]
+                      (str/split line #","))                ;splitting columns into seperate strings
+                    people)
+        header (first people)                               ;stuff in header acts as keys
+        people (rest people)                                ;stuff in people acts as map values
+        people (map (fn [line]
+                      (interleave header line))             ;makes the header line the key
+                    people)                                 ;;makes people the 2nd argument and the values
+        people (map (fn [line]
+                      (apply hash-map line))                ;setting has-map keys to values
+                      people)
+        people (walk/keywordize-keys people)                ;make string keys into clojure keywords
+        people (filter (fn [line]
+                         (= "Brazil" (:country line)))      ;Filter through countries with "Brazil" :country is keyword
+                       people)
+        ]
+         (spit "filtered_people.edn"                        ;spit data into a new clojure file
+               (pr-str people)
+               )                                            ;end of spit
+          )                                                 ;end of let
+  )                                                         ;end of main
